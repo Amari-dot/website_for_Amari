efe3932523170ad9a15bb638a985d300a01ba823
Amari Owens <amaow15@gmail.com>
2024-08-18 13:30:07 -0400

    my first initial commit to my website

---
+++ index.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+        x.className += "responsive";
+        
+    }else{
+        x.className = "topnav";
+    }
+}
