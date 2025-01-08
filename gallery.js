18d3c96389ba0dc9eff443e93f39d2fadd516335
Amari Owens <amaow15@gmail.com>
2025-01-07 22:52:05 -0500

    made a commit to modify text

--- gallery.js
+++ gallery.js
@@ -3,8 +3,9 @@
 
 
 function changeFont(event){
-    event.target.style.fontFamily = 'Arial';
+    event.target.style.fontFamily = 'Times New Roman';
     event.target.style.backgroundColor = 'yellow';
+    event.target.innerHTML = 'Thanks';
 };
 
 changeT.addEventListener("click",changeFont);
