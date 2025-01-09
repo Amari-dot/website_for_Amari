eb9401d75fe07295bd3a4450ee236223e10c4f1b
Amari Owens <amaow15@gmail.com>
2025-01-08 23:23:01 -0500

    figured out the hover trick

--- gallery.js
+++ gallery.js
@@ -5,7 +5,7 @@
 function changeFont(event){
     event.target.style.fontFamily = 'Times New Roman';
     event.target.style.backgroundColor = 'yellow';
-    event.target.innerHTML = 'Thanks';
+    event.target.styinnerHTML = 'Thanks';
 };
 
 changeT.addEventListener("click",changeFont);
